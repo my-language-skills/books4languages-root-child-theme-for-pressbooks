2f358c5d7567cdc89afc1e5bff5d4ea6592a5ad6
Zhitnitskii Daniil <danonchik98@gmail.com>
2018-09-17 16:47:10 +0200

    changed logo in login page

---
+++ functions.php
@@ -0,0 +1,16 @@
+<?php
+
+
+function pbrc_login_logo() { ?>
+    <style type="text/css">
+        #login h1 a, .login h1 a {
+            background-image: url(<?php echo get_stylesheet_directory_uri(); ?>/assets/images/header-logo.png) !important;
+		height:65px;
+		width:320px;
+		background-size: 320px 65px;
+		background-repeat: no-repeat;
+        	padding-bottom: 30px;
+        }
+    </style>
+<?php }
+add_action( 'login_enqueue_scripts', 'pbrc_login_logo', 1000 );
